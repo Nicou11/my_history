e3d9dc8869faa54d1eed6fe4dce6c7384be71936
Nicou11 <lyoungin1204@gmail.com>
2024-07-24 09:25:05 +0900

    git

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import sys
 
+INPUT = sys.argv[1]
+
 def cnt():
-    INPUT = sys.argv[1]
+    #INPUT = sys.argv[1]
     df = pd.read_parquet("~/tmp/history.parquet")
     fdf = df[df['cmd'].str.contains(INPUT)]
     cnt = fdf['cnt'].sum()
