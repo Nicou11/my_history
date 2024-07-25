cff6b8505f8104cc10f8f3f29ff0934af268839c
Nicou11 <lyoungin1204@gmail.com>
2024-07-25 10:05:18 +0900

    - [ ] 아래와 같은 사용법의 프로그램을 만들자 - [ ] 사용법을 작성하자 ``` $ my-history -s ls ls 사용 횟수는 1234회 입니다.
    $ my-history -t 10 -d 2024-07-17
      cmd  cnt
    pyenv 4256
       cd 3472
      git 3396
    mkdir 1932
      pip 1592
      cat 1368
       vi 1356
     sudo 1320
      pdm 1220
       rm 1104
    ```

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # my_history
 
+## Require
+```
+[ ] 아래와 같은 사용법의 프로그램을 만들자
+[ ] 사용법을 작성하자
+$ my-history -s ls
+ls 사용 횟수는 1234회 입니다.
+
+$ my-history -t 10 -d 2024-07-17
+  cmd  cnt
+pyenv 4256
+   cd 3472
+  git 3396
+mkdir 1932
+  pip 1592
+  cat 1368
+   vi 1356
+ sudo 1320
+  pdm 1220
+   rm 1104
+```
+
 ## Usage
 ```
 $ search <검색할 단어>
--- src/my_history/mh.py
+++ src/my_history/mh.py
@@ -4,8 +4,22 @@ import argparse
 
 #INPUT = sys.argv[1]
 
+def argp():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--count", action="store_true", help="mh -c <cmd>")
+    group.add_argument("-t", "--top", action="store_true", help="mh -t <YYYY-MM-DD>")
 
-def sort(n, date):
+    args = parser.parse_args()
+    
+    if args.count:
+        abc(args.count)
+    elif args.top:
+        xyz(args.top)
+    else:
+        print("사용법 확인")
+
+def cnt(n, date):
     #df = read_parquet()
     df = read_parquet("~/data/parquet")
     fdf = df[df['dt'] == date]
@@ -13,17 +27,11 @@ def sort(n, date):
     ddf = sdf.drop(columns=['dt'])
     print(ddf.to_string(index=False))
 
-def cnt(q): 
-    df = read_parquet()
-    fdf = df[df['cmd'].str.contains(q)]
-    cnt = fdf['cnt'].sum()
-    return cnt
-
 def read_parquet(path="~/tmp/history.parquet"):
     df = pd.read_parquet(path)
     return df
 
-def query():
+def query(q):
     q = sys.argv[1]
     i = cnt(q)
     print(f'{q} 사용 횟수는 {i}회 입니다.')
