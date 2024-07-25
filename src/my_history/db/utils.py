0585af2edfdae746f0c08f40a3310cc0a190045f
Nicou11 <lyoungin1204@gmail.com>
2024-07-25 15:29:53 +0900

    u add

--- src/my_history/db/utils.py
+++ src/my_history/db/utils.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
+def read_data(path="~/data/parquet"):
+    df = pd.read_parquet(path)
+    return df
+
 def top(n, date):
-    df = pd.read_parquet("~/data/parquet")
+    df = read_data()
     fdf = df[df['dt'] == date]
     sdf = fdf.sort_values(by='cnt', ascending=False).head(n)
     ddf = sdf.drop(columns=['dt'])
 
     r = ddf.to_string(index=False)
     return r
+
+def count(query):
+    df = read_data()
+    fdf = df[df['cmd'] == query]
+    cnt = fdf['cnt'].sum()
+    print(f'{q} 사용 횟수는 {cnt}회 입니다.')
